d57ff38629091c1ff4edba516b8460e53bd7c5e0
ei-sugimoto <eikun1014@gmail.com>
2024-01-29 16:21:47 +0000

    helloworld

---
+++ main.cu
@@ -0,0 +1,11 @@
+#include <iostream>
+
+__global__ void helloWorld() {
+    printf("Hello, World!\n");
+}
+
+int main() {
+    helloWorld<<<1, 1>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
